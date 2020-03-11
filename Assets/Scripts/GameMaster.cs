3ec4a03c4c575e52d01ef91af0c1ff90ec764e71
Samntorr <48298970+Samntorr@users.noreply.github.com>
2020-03-10 21:25:01 -0400

    Added GameMaster, quits in build and hides cursor

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //hides cursor
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Quits game in builds not in untiy
+        if (Input.GetButtonUp("Cancel"))
+        {
+            Application.Quit();
+        }
+    }
+}
