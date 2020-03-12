3344166773eb1b4637142ec23f9691cbfa756889
mynameisfloog <48299534+mynameisfloog@users.noreply.github.com>
2020-03-11 23:33:29 -0400

    AI works better
    Now it randomly chooses a direction and goes a set distance. Need to work on adjusting length of walk and other tweaks

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -9,6 +9,7 @@ public class AIController : MonoBehaviour
     int pauseTime = 1;
     float smallPause = 0.03f;
     [SerializeField()] float moveSpeed;
+    bool hasNumber = false;
 
     Vector3 pos;
     
@@ -23,54 +24,65 @@ public class AIController : MonoBehaviour
         float WALKIN = walkTime;
         while (WALKIN > 0)
         {
-            direction = Random.Range(1, 8); // Randomly pick a direction
-            /* Directions follow numpad notation, but 6 is 5 and every number after is bumped up, like so:
-             * 6 7 8
-             * 4 * 5
-             * 1 2 3  */
-            Debug.Log(direction);
+            if (hasNumber == false)
+            {
+                direction = Random.Range(1, 8); // Randomly pick a direction
+                /* Directions follow numpad notation, but 6 is 5 and every number after is bumped up, like so:
+                 * 6 7 8
+                 * 4 * 5
+                 * 1 2 3  */
+                Debug.Log(direction);
+            }
             if (direction == 1) //Southwest
             {
                 pos -= transform.up * Time.deltaTime * moveSpeed;
                 pos -= transform.right * Time.deltaTime * moveSpeed;
+                hasNumber = true;
                 yield return new WaitForSeconds(smallPause);
             }
             if (direction == 2) //South
             {
                 pos -= transform.up * Time.deltaTime * moveSpeed;
+                hasNumber = true;
                 yield return new WaitForSeconds(smallPause);
             }
             if (direction == 3) //Southeast
             {
                 pos -= transform.up * Time.deltaTime * moveSpeed;
                 pos += transform.right * Time.deltaTime * moveSpeed;
+                hasNumber = true;
                 yield return new WaitForSeconds(smallPause);
             }
             if (direction == 4) //West
             {
                 pos -= transform.right * Time.deltaTime * moveSpeed;
+                hasNumber = true;
                 yield return new WaitForSeconds(smallPause);
             }
             if (direction == 5) //East
             {
                 pos += transform.right * Time.deltaTime * moveSpeed;
+                hasNumber = true;
                 yield return new WaitForSeconds(smallPause);
             }
             if (direction == 6) //Northwest
             {
                 pos += transform.up * Time.deltaTime * moveSpeed;
                 pos -= transform.right * Time.deltaTime * moveSpeed;
+                hasNumber = true;
                 yield return new WaitForSeconds(smallPause);
             }
             if (direction == 7) //North
             {
                 pos += transform.up * Time.deltaTime * moveSpeed;
+                hasNumber = true;
                 yield return new WaitForSeconds(smallPause);
             }
             if (direction == 8) //Northeast
             {
                 pos += transform.up * Time.deltaTime * moveSpeed;
                 pos -= transform.right * Time.deltaTime * moveSpeed;
+                hasNumber = true;
                 yield return new WaitForSeconds(smallPause);
             }
             transform.position = pos;
@@ -78,5 +90,6 @@ public class AIController : MonoBehaviour
         }
         
         yield return new WaitForSeconds(pauseTime);
+        hasNumber = false;
     }
 }
