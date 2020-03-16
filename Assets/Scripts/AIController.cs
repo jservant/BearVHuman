822f760bc1b2e6cd80b32b398c35c6cc02f770a6
mynameisfloog <48299534+mynameisfloog@users.noreply.github.com>
2020-03-16 14:18:28 -0400

    AI WORKS
    It's identical to Hidden in Plain Sight but needs to be spruced up a bit. For now though this is the first push where it works as intended

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class AIController : MonoBehaviour
 {
-    int direction; //For one of 8 directions
-    public float walkTime;
-    int pauseTime = 1;
-    float smallPause = 0.03f;
-    [SerializeField()] float moveSpeed;
-    bool hasNumber = false;
+    [Header("Travelling")]
+    [SerializeField] float moveSpeed;    // Determines how fast the AI gets to their desired endpoint
+    [SerializeField] float walkDistance; // Determines distance the AI travels
+    
+    [Header("Time Walking")]
+    [SerializeField] int pauseTime;
+    [SerializeField] float smallPause = 0.03f;
+
+    [Header("Debug (DO NOT EDIT)")]
+    [SerializeField] bool hasNumber = false; // Checks to see if a direction has been assigned to the AI
+    [SerializeField] int direction; //For one of 8 directions    
 
     Vector3 pos;
     
@@ -21,10 +26,10 @@ public class AIController : MonoBehaviour
 
     IEnumerator MoveAI()
     {
-        float WALKIN = walkTime;
+        float WALKIN = walkDistance; //Creates a new float so it doesn't actually modify the walkDistance var
         while (WALKIN > 0)
         {
-            if (hasNumber == false)
+            if (hasNumber == false) // If the a AI doesn't have a direction...
             {
                 direction = Random.Range(1, 8); // Randomly pick a direction
                 /* Directions follow numpad notation, but 6 is 5 and every number after is bumped up, like so:
@@ -85,11 +90,16 @@ public class AIController : MonoBehaviour
                 hasNumber = true;
                 yield return new WaitForSeconds(smallPause);
             }
+            
             transform.position = pos;
-            WALKIN -= Time.deltaTime;
+            WALKIN -= Time.deltaTime; // Time goes down while moving
         }
-        
-        yield return new WaitForSeconds(pauseTime);
-        hasNumber = false;
+
+        if (WALKIN <= 0)
+        {
+            yield return new WaitForSeconds(pauseTime);
+            hasNumber = false;
+            StartCoroutine(MoveAI());
+        }
     }
 }
