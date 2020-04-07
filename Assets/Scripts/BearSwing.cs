63028da057a09d84495ee1b582a583c15714ef7f
mynameisfloog <48299534+mynameisfloog@users.noreply.github.com>
2020-04-06 22:05:08 -0400

    jk NOW swinging works
    it only worked on the playable human before

--- Assets/Scripts/BearSwing.cs
+++ Assets/Scripts/BearSwing.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LosingTries : MonoBehaviour
+public class BearSwing : MonoBehaviour
 {
-    public string pHumanTag;
     public string HumanTag;
+    [SerializeField] float despawnTime;
+
+    void Update()
+    {
+        despawnTime -= Time.deltaTime;
+        if (despawnTime <= 0) { Destroy(gameObject); }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag(HumanTag) || other.CompareTag(pHumanTag))
+        if (other.CompareTag(HumanTag))
         {
             Destroy(other.gameObject);
             Destroy(gameObject);
